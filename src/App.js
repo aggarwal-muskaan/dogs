ff618f85f79d136a8a6adb9f92177d5b0cf6d4c7
Muskaan Aggarwal <muskaan19010@gmail.com>
2020-12-10 15:09:35 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import React, { Component } from "react";
+import DogList from "./DogList";
+import "./App.css";
+import { Route, Switch } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  static defaultProps = {
+    dogs: [
+      {
+        name: "Bonzo",
+        age: 2,
+        src: "bonzo",
+        facts: [
+          "Bonzo has soo much energy!",
+          "People call Bonzo 'vodafone wala kutta'",
+          "Bonzo is highly intelligent.",
+        ],
+      },
+      {
+        name: "Galaxy",
+        age: 1,
+        src: "galaxy",
+        facts: [
+          "Galaxy is toooo friendly",
+          "Galaxy wants to cuddle with you!",
+          "Galaxy has two pony tails and a puff",
+        ],
+      },
+      {
+        name: "Reactar",
+        age: 4,
+        src: "reactar",
+        facts: [
+          "Reactor has got his name from a stranger",
+          "Reactar loves people more than dogs",
+          "Reactor get happy when anyone talks to him.",
+        ],
+      },
+      {
+        name: "Romeo",
+        age: 6,
+        src: "romeo",
+        facts: [
+          "Romeo loves to eat Sponge-Rasgulla",
+          "Romeo gives you hug from behind.",
+          "This Romeo don't chase 'Juliet'",
+        ],
+      },
+      // {
+      //   name: "Skippy",
+      //   age: 3,
+      //   src: "skippy",
+      //   facts: ["Skippy has ", "Skippy loves high jumping.", ""],
+      // },
+    ],
+  };
+  render() {
+    return (
+      <Route path="/dogs" render={() => <DogList dogs={this.props.dogs} />} />
+    );
+  }
 }
 
 export default App;
